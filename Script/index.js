00e6a002dcc0d63bada695178c44e41d5f46929c
bayjidalom <bayjidalom1005@gmail.com>
2026-03-02 16:07:11 +0600

    Page visibility Logic

--- Script/index.js
+++ Script/index.js
@@ -3,8 +3,12 @@ let currentTab = "all";
 const tabActive = ['bg-sky-600', 'text-white'];
 const tabInactive = ['bg-white', 'text-gray-600'];
 
-function switchTab(tab){
-    const tabs =["all", "interview", "rejected"];
+const allContainer = document.getElementById("all-container");
+const interviewContainer = document.getElementById("interview-container");
+const rejectedContainer = document.getElementById("reject-container");
+
+function switchTab(tab) {
+    const tabs = ["all", "interview", "rejected"];
 
     for (const t of tabs) {
         const tabName = document.getElementById("tab-" + t);
@@ -14,6 +18,21 @@ function switchTab(tab){
         } else {
             tabName.classList.remove(...tabActive);
             tabName.classList.add(...tabInactive);
+        }
+    }
+
+    const pages = [allContainer, interviewContainer, rejectedContainer];
+    for (const section of pages) {
+        section.classList.add("hidden");
+
+        if (tab === "all") {
+            allContainer.classList.remove("hidden");
+        }
+        if (tab === "interview") {
+            interviewContainer.classList.remove("hidden");
+        }
+        if (tab === "rejected") {
+            rejectedContainer.classList.remove("hidden");
         }
     }
 }
