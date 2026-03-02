6cbcfb9183ba483979848c1f0ad915656242b769
bayjidalom <bayjidalom1005@gmail.com>
2026-03-02 15:11:43 +0600

    Card Added

---
+++ try.js
@@ -0,0 +1,141 @@
+let currentTab = "all";
+
+const tabActive = ['bg-sky-600', 'text-white']
+const tabInactive = ['bg-white', 'text-gray-600']
+
+const allContainer = document.getElementById("all-container");
+const interviewContainer = document.getElementById("interview-container");
+const rejectedContainer = document.getElementById("reject-container");
+console.log(allContainer, interviewContainer, rejectedContainer);
+
+const emptyState = document.getElementById("empty-state");          //👈
+const availableStat = document.getElementById("available");         //⚪
+
+function switchTab(tab) {
+    // console.log(tab)     // all , interview , rejected
+    const tabs = ["all", "interview", "rejected"];
+
+    currentTab = tab;                                //⚪
+
+    //--01
+    for (const t of tabs) {
+        const tabName = document.getElementById("tab-" + t)
+        if (t === tab) {
+            tabName.classList.remove(...tabInactive);
+            tabName.classList.add(...tabActive)
+        } else {
+            tabName.classList.remove(...tabActive)
+            tabName.classList.add(...tabInactive)
+        }
+
+        updateStat()                        //⚪
+    }
+
+    //--02
+    const pages = [allContainer, interviewContainer, rejectedContainer];
+    for (const section of pages) {
+        section.classList.add("hidden");
+
+        // emptyState.classList.add("hidden")        //👈  
+
+    }
+    emptyState.classList.add('hidden')          //👈
+
+    if (tab === "all") {
+        allContainer.classList.remove("hidden")
+        if (allContainer.children.length < 1) {
+            emptyState.classList.remove("hidden");         //👈
+        }
+    }
+    else if (tab === "interview") {
+        interviewContainer.classList.remove("hidden")
+        if (interviewContainer.children.length < 1) {
+            emptyState.classList.remove('hidden');          //👈
+
+        }
+    }
+    else {
+        rejectedContainer.classList.remove("hidden");
+        if (rejectedContainer.children.length < 1) {
+            emptyState.classList.remove('hidden');          //👈
+
+        }
+    }
+}
+
+const totalStat = document.getElementById("stat-total")
+const interviewStat = document.getElementById("stat-interview")
+const rejectStat = document.getElementById("stat-reject")
+
+// totalStat.innerText = allContainer.children.length;
+
+switchTab(currentTab);       // call korle all selected dekhabe
+
+//--03
+document.getElementById("jobs-container").addEventListener("click", function (event) {
+    const clickedElement = event.target;
+    // console.log(clickedElement.parentNode.parentNode.parentNode.parentNode)    //Not safe it
+    const card = clickedElement.closest(".card");
+    
+    
+    const parent = card.parentNode;                       //👈delete korte
+    const status = card.querySelector(".status");                  //👈
+
+
+
+    if (clickedElement.classList.contains("interview")) {
+        status.innerText = "interviewed";                       //👈
+        interviewContainer.appendChild(card);
+        // updateStat()          // last e
+
+    }
+    if (clickedElement.classList.contains("rejected")) {
+        status.innerText = "Rejected";                         //👈
+        rejectedContainer.appendChild(card);
+        // updateStat()  //last e
+
+
+    }
+    if (clickedElement.classList.contains("delete")) {
+        // interviewContainer.appendChild(card);
+        // console.log(parent)
+        parent.removeChild(card)                        //👈
+        // updateStat() // last e
+
+    }
+
+    updateStat();      //🟢ut e called
+})
+
+// 37m-47m
+
+function updateStat() {
+    // totalStat.innerText = allContainer.children.length;
+    // interviewStat.innerText = interviewContainer.children.length;
+    // rejectStat.innerText = rejectedContainer.children.length;
+
+    const counts = {
+        all: allContainer.children.length,
+        interview: interviewContainer.children.length,     //⚪
+        rejected: rejectedContainer.children.length,
+    };
+
+    totalStat.innerText = counts.all;
+    interviewStat.innerText = counts.interview;
+    rejectStat.innerText = counts.rejected;                 //⚪
+
+    availableStat.innerText = counts[currentTab];        //⚪
+
+    if (counts[currentTab] < 1) {
+        emptyState.classList.remove("hidden")
+    } else {
+        emptyState.classList.add("hidden")
+
+    }
+
+}
+
+updateStat()
+
+
+// 47-57m
